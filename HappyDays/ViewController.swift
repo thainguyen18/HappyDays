b5cf333a45964cb0eae63cd4352afd5ecb3763b6
Thai Nguyen <thaiomega3@gmail.com>
2019-10-06 20:18:11 -0500

    Initial Commit

---
+++ HappyDays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyDays
+//
+//  Created by Thai Nguyen on 10/6/19.
+//  Copyright © 2019 Thai Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
